6c1912f5981eb3cae02b83d2a6705910ef199ff0
mary_berezhna <48996884+maryberezhna@users.noreply.github.com>
2026-04-20 12:45:04 +0300

    Add RootLayout component and metadata for app

---
+++ app/global.css
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Можливості для дитини',
+  description: 'Усі програми, курси, стипендії та підтримка для дітей 0-18 років в Україні',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="uk">
+      <body>{children}</body>
+    </html>
+  );
+}
