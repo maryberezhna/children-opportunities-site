6d1bbb74d2c45322278b7c6c08e3da67aee974bd
mary_berezhna <48996884+maryberezhna@users.noreply.github.com>
2026-04-20 14:46:45 +0300

    Update Analytics.js

--- app/Analytics.js
+++ app/Analytics.js
@@ -1,6 +1,23 @@
 'use client';
-import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
+import Script from 'next/script';
+
+const GA_ID = 'G-KPLE8LGH91';
 
 export function Analytics() {
-  return <VercelAnalytics />;
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+    </>
+  );
 }
